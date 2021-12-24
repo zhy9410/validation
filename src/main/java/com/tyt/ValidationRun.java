f52ba27a03b34d399d9d6ca19bf1ef061c9a6c3c
zhy9410 <1064630810@qq.com>
2021-12-24 11:10:41 +0800

    项目初始化

---
+++ src/main/java/com/tyt/ValidationRun.java
@@ -0,0 +1,17 @@
+package com.tyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhy
+ * @date 2021/12/24 10:59
+ * @Description 启动类
+ */
+@SpringBootApplication
+public class ValidationRun {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationRun.class, args);
+        System.out.println("-------项目启动--------");
+    }
+}
